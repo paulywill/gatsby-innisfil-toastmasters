bc219765cec662d46d729736437050563a688ac8
Paul Gamble <pgamble@gmail.com>
2020-05-20 20:43:07 +0000

    changes for testing; changes for github-pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,13 @@
 import React from "react"
 import { Link } from "gatsby"
-
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>What's happening</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
+    <h1>What's happening... This is a test!</h1>
+    
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
